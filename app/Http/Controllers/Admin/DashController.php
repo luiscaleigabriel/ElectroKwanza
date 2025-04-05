f7fb8458bc6d0258ee0ee34d0788f9c6d7f76514
Luís Silvério Calei Gabriel <132252803+luiscaleigabriel@users.noreply.github.com>
2025-04-05 16:27:58 +0100

    ..

--- app/Http/Controllers/Admin/DashController.php
+++ app/Http/Controllers/Admin/DashController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashController extends Controller
 {
-    public function index()
+    public function index(Order $order, User $user)
     {
-        return view('admin.dashboard');
+        $orders = $order->all();
+        $users = $user->all();
+
+        dd(count($users));
+        return view('admin.dashboard', compact('orders', 'users'));
     }
 }
